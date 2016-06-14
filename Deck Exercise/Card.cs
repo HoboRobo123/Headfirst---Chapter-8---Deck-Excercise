f6f4a24aaeb3a9d89e08b5140227b7ffc3964e7f
HoboRobo123 <arobilla@hotmail.com>
2016-06-13 20:13:18 -0400

    Initial Commit

---
+++ Deck Exercise/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards_Exercise_2
+{
+    class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public string Name
+        {
+            get
+            {
+                return Value.ToString() + " of " + Suit.ToString();
+            }
+        }
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
